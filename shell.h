4f5bab56fb7b7b38a42e063dbff4a8861d443f9c
SabiBola <sabi.vibess@gmail.com>
2023-10-10 06:54:38 -0700

    Shell Header and Readme

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef shell.h
+#define shell.h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
